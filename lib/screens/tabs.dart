db7aef976d986eda4fe1c94329d499799bfc7f8f
Ian Mabans <kamauian614@gmail.com>
2023-05-09 08:30:34 +0300

    Using a provider to depend on other providers

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/providers/meals_provider.dart';
+
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/providers/favorites_provider.dart';
@@ -26,8 +26,6 @@ class TabScreen extends ConsumerStatefulWidget {
 class _TabScreenState extends ConsumerState<TabScreen> {
   int _selectedPageIndex = 0;
 
-
-
   void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
@@ -36,8 +34,6 @@ class _TabScreenState extends ConsumerState<TabScreen> {
           builder: (context) => const FiltersScreen(),
         ),
       );
-
-
     }
   }
 
@@ -49,23 +45,7 @@ class _TabScreenState extends ConsumerState<TabScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(favoriteMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
